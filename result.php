1ab8ba62392f9aa93f121cb1f8b8796142ecd001
Jonas <Jonas@145.44.90.149>
2017-05-09 16:38:58 +0200

    Google opgedeeld

--- result.php
+++ result.php
@@ -1,17 +1,18 @@
 <?php
 
+include "autoload.php";
+
 use modules\TreinReis;
 use modules\KoerierReis;
 
 $treinReis = new TreinReis();
 $koerierReis = new KoerierReis();
 
-$afstandNaarStation1 = $treinReis->getDistanceToStation($_POST['origin']);
-$afstandNaarStation2 = $treinReis->getDistanceToStation($_POST['destination']);
+$afstandNaarStation1 = $treinReis->berekenAfstand($_POST['origin']);
+$afstandNaarStation2 = $treinReis->berekenAfstand($_POST['destination']);
 $afstandPerBus = $koerierReis->berekenAfstand($_POST['origin'], $_POST['destination']);
-echo "Begin naar station: " . $afstandNaarStation1 . "m<br>";
-echo "Station naar eind: " . $afstandNaarStation2 . "m<br>";
-echo "Direct: " . $afstandPerBus . "m";
+echo "Begin naar station: " . $afstandNaarStation1 . "km<br>";
+echo "Station naar eind: " . $afstandNaarStation2 . "km<br>";
+echo "Direct: " . $afstandPerBus . "km";
 exit();
-$api_key = "";
 ?> 
