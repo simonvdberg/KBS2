fb57e00dc424a3c211b8a0db33f80e960e74f433
svdberg <svdberg@EC-SVDB.ELEVATION.LOCAL>
2017-05-09 15:21:09 +0200

    Basis voor berekening tarieven

--- index.php
+++ index.php
@@ -5,3 +5,15 @@
         <input type="submit" value="verzend" />
     </form>
 </html>
+
+<?php
+    include "model/Koerier.php";
+    include "model/Tarief.php";
+    
+    $fietsKoerier = new model/Koerier("Fietskoerier",  array(
+        new model/Tarief(9, 0, 4),
+        new model/Tarief(14, 0, 8),
+        new model/Tarief(19, 0, 12),
+        new model/Tarief(15, 0.56, 12),
+        ) );
+    $fietsKoerier->berekenTarief();
