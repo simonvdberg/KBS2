bb9a35d478e7b2ca9f5a2b6808d18153320e5e64
svdberg <svdberg@EC-SVDB.ELEVATION.LOCAL>
2017-05-15 16:31:15 +0200

    Test code toegevoegd voor connectie met database.

--- index.php
+++ index.php
@@ -15,11 +15,35 @@ use model\Model;
 use model\Koerier;
 use model\Tarief;
 
+$servername = "localhost";
+$username = "root";
+// Create connection
+// Port configuratie nodig vanwege gebruik van 2 MySQL clients
+$mysqli = mysqli_connect($servername, $username, null, "tzt", 3307);
+
+// Check connection
+if (!$mysqli) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+if ($result=mysqli_query($mysqli,"select * from koerier"))
+  {
+  // Fetch one and one row
+  while ($row=mysqli_fetch_row($result))
+    {
+    printf ("%s (%s)\n",$row[0],$row[1]);
+    }
+  // Free result set
+  mysqli_free_result($result);
+}
+
+mysqli_close($mysqli);
+
 $fietsKoerier = new Koerier("Fietskoerier", array(
     new Tarief(9, 0, 4),
     new Tarief(14, 0, 8),
     new Tarief(19, 0, 12),
-    new Tarief(15, 0.56, 0)
-        ));
+    new Tarief(15, 0.56, 0)));
 
-echo $fietsKoerier->berekenTarief(4);
+$fietsKoerier->berekenTarief(4);
