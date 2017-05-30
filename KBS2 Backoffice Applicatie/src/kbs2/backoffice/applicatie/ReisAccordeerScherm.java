af43706c94f14b7060b69a93fb38abc5db03b1f9
svdberg <svdberg@EC-SVDB.ELEVATION.LOCAL>
2017-05-30 14:55:34 +0200

    Jar toegevoegd

--- KBS2 Backoffice Applicatie/src/kbs2/backoffice/applicatie/ReisAccordeerScherm.java
+++ KBS2 Backoffice Applicatie/src/kbs2/backoffice/applicatie/ReisAccordeerScherm.java
@@ -19,7 +19,7 @@ import javax.swing.JLabel;
  *
  * @author svdberg
  */
-public class ReisAccordeerScherm extends JFrame implements ActionListener{
+public class ReisAccordeerScherm extends JFrame implements ActionListener {
 
     private JButton home;
     private JButton help;
@@ -31,22 +31,32 @@ public class ReisAccordeerScherm extends JFrame implements ActionListener{
     private JLabel pakketreferentie;
     private JLabel klantGegevens;
 
-    public ReisAccordeerScherm()  {
+    public ReisAccordeerScherm() {
         setLayout(new GridBagLayout());
         GridBagConstraints grid = new GridBagConstraints();
+
         home = new JButton();
-        grid.fill = GridBagConstraints.HORIZONTAL;
+      //  grid.fill = GridBagConstraints.HORIZONTAL;
         grid.gridx = 0;
         grid.gridy = 0;
+
+        banner = new JLabel("Accorderen uitgevoerde activiteiten");
+      
+      //  grid.fill = GridBagConstraints.HORIZONTAL;
+        grid.gridx = 1;
+        grid.gridy = 0;
+          add(banner, grid);
+        
         //TODO svdberg. Path werkt waarschijnlijk niet op Linux.
-        ImageIcon imageIcon = new ImageIcon(new ImageIcon("src\\icons\\home-icon.png").getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT));
-        home.setIcon(imageIcon);
+        ImageIcon homeIcon = new ImageIcon(new ImageIcon("src\\icons\\home-icon.png").getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT));
+        home.setIcon(homeIcon);
         add(home, grid);
         home.addActionListener(this);
-        help = new JButton("help");
-        grid.fill = GridBagConstraints.HORIZONTAL;
-        grid.gridx = 0;
-        grid.gridy = 1;
+
+        help = new JButton();
+    //    grid.fill = GridBagConstraints.HORIZONTAL;
+        grid.gridx = 2;
+        grid.gridy = 0;
         add(help, grid);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -56,7 +66,7 @@ public class ReisAccordeerScherm extends JFrame implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource().equals(home)){
+        if (e.getSource().equals(home)) {
             dispose();
             Tabel tabel = new Tabel();
             tabel.setVisible(true);
