1c5fa2c1095971350e220ffceee62a1886c0637b
svdberg <svdberg@EC-SVDB.ELEVATION.LOCAL>
2017-05-30 20:52:31 +0200

    Verschrikkelijke swing UI toegevoegd voor accorderen van bezorgopdrachten

---
+++ KBS2 Backoffice Applicatie/src/kbs2/backoffice/applicatie/TrajectRecord.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kbs2.backoffice.applicatie;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.TableModel;
+
+/**
+ *
+ * @author svdberg
+ */
+public class TrajectRecord extends JPanel {
+    private TableModel model;
+
+    public TrajectRecord() {
+        
+        String kolomNamen[] = {"Traject", "Startpunt", "Eindpunt", "Uitvoerder", "Afstand", "Inkoopkosten", "Aflevermoment", "Status", "Akkoord", "Moment"};
+        Object data[][] = {
+            {"1", "Barneveld", "Zwolle", "Fietskoeriers", "3812 meter", "9,00", "28-05-17, 16:31", "Afgeleverd", "JA", "30-05-17, 08:17"}
+        };
+        model = new NietBewerkbaarModel(data, kolomNamen);
+        
+        setLayout(new FlowLayout());
+        JTable table = new JTable();
+        table.setModel(model);
+        table.setPreferredScrollableViewportSize(new Dimension(1100, 16));
+        table.setFillsViewportHeight(true);
+        JScrollPane scrollPane = new JScrollPane(table);
+        add(scrollPane);
+    }
+
+}
