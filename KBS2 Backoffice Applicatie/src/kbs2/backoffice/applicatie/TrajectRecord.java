a1f0e38ba5b0a126d028c565a432f7abd21ab6ae
svdberg <svdberg@EC-SVDB.ELEVATION.LOCAL>
2017-06-05 16:59:41 +0200

    DB Fix en wat java files

--- KBS2 Backoffice Applicatie/src/kbs2/backoffice/applicatie/TrajectRecord.java
+++ KBS2 Backoffice Applicatie/src/kbs2/backoffice/applicatie/TrajectRecord.java
@@ -6,7 +6,8 @@
 package kbs2.backoffice.applicatie;
 
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -17,23 +18,55 @@ import javax.swing.table.TableModel;
  * @author svdberg
  */
 public class TrajectRecord extends JPanel {
+
     private TableModel model;
 
+    private JPanel top;
+    private JPanel bot;
+
+    private JButton accordeer;
+    private JButton wijsAf;
+    
+    private String referentie;
+    private String startpunt;
+    private String eindpunt;
+    private String koerier;
+    private String afstand;
+    private String prijs;
+    private String aflevermoment;
+    private String Status;
+
     public TrajectRecord() {
-        
+        setLayout(new GridLayout(2, 0));
         String kolomNamen[] = {"Traject", "Startpunt", "Eindpunt", "Uitvoerder", "Afstand", "Inkoopkosten", "Aflevermoment", "Status", "Akkoord", "Moment"};
         Object data[][] = {
             {"1", "Barneveld", "Zwolle", "Fietskoeriers", "3812 meter", "9,00", "28-05-17, 16:31", "Afgeleverd", "JA", "30-05-17, 08:17"}
         };
         model = new NietBewerkbaarModel(data, kolomNamen);
-        
-        setLayout(new FlowLayout());
+
+        top = new JPanel();
+        bot = new JPanel();
+
+        add(top);
+        add(bot);
+
         JTable table = new JTable();
         table.setModel(model);
         table.setPreferredScrollableViewportSize(new Dimension(1100, 16));
         table.setFillsViewportHeight(true);
         JScrollPane scrollPane = new JScrollPane(table);
-        add(scrollPane);
+
+        top.add(scrollPane);
+
+        accordeer = new JButton("Accordeer");
+        wijsAf = new JButton("Wijs af");
+
+        bot.add(accordeer);
+        bot.add(wijsAf);
+    }
+
+    TrajectRecord(int trajectId1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
--- tzt-koerier-tarief-aanmaken.sql
+++ tzt-koerier-tarief-aanmaken.sql
@@ -1,39 +1,39 @@
+INSERT INTO koerier
+VALUES(null, "De Fietskoeriers", 1);
 
-INSERT INTO Koerier
-VALUES(null, "De Fietskoeriers");
-
-INSERT INTO Tarief
+INSERT INTO tarief
 VALUES(null, 1, 9, 0, 4 );
 
-INSERT INTO Tarief
+INSERT INTO tarief
 VALUES(null, 1, 14, 0, 8 );
 
-INSERT INTO Tarief
+INSERT INTO tarief
 VALUES(null, 1, 19, 0, 12 );
 
-INSERT INTO Tarief
+INSERT INTO tarief
 VALUES(null, 1, 15, 0.56, 0 );
 
-INSERT INTO Koerier
-VALUES(null, "Bodekoeriers");
+INSERT INTO koerier
+VALUES(null, "Bodekoeriers", 0);
 
-INSERT INTO Tarief
+INSERT INTO tarief
 VALUES(null, 2, 12.5, 0, 40 );
 
-INSERT INTO Tarief
+INSERT INTO tarief
 VALUES(null, 2, 0, 0.4, 0 );
 
-INSERT INTO Koerier
-VALUES(null, "Pietersen Transport BV");
+INSERT INTO koerier
+VALUES(null, "Pietersen Transport BV", 0);
 
-INSERT INTO Tarief
+INSERT INTO tarief
 VALUES(null, 3, 10, 0, 25 );
 
-INSERT INTO Tarief
+INSERT INTO tarief
 VALUES(null, 3, 0, 0.39, 0 );
 
+
 INSERT INTO Koerier
-VALUES(null, "Treinkoerier");
+VALUES(null, "Treinkoerier", 0);
 
 INSERT INTO Tarief
 VALUES(null, 4, 2, 0, 999999);
